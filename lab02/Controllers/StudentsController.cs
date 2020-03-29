14c3b3ab3df6b0f2591fd2e50bb548716a585094
SandraRawicz <sandra.rawicz@gmail.com>
2020-03-29 14:18:10 +0200

    zad 05

--- lab02/Controllers/StudentsController.cs
+++ lab02/Controllers/StudentsController.cs
@@ -11,9 +11,9 @@ namespace lab02.Controllers
     public class StudentsController : ControllerBase
     {
         [HttpGet]
-        public string GetStudent()
+        public string GetStudent(string orderBy)
         {
-            return "Kowalski, Malewski, Andrzejewski";
+            return $"Kowalski, Malewski, Andrzejewski sortowaanie = {orderBy}";
         }
 
         [HttpGet("{id}")]
